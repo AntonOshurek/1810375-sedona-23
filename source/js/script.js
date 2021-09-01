582afea07bf7186fb6feb4ecbcb71279874104b6
AntonOshurek <oshurekweb@gmail.com>
2021-09-01 13:57:43 +0200

    add modal for forms page

--- source/js/script.js
+++ source/js/script.js
@@ -23,3 +23,41 @@ closeMenu();
     menuList.classList.remove('navigation__list--active');
   }
 
+//form modals
+
+const body = document.querySelector('.body');
+const modalError = document.querySelector('.form-modal--error');
+const modalSuccess = document.querySelector('.form-modal--success');
+const modalBg = document.querySelectorAll('.modal__bg');
+
+
+const modalErrorBtn = document.querySelector('.form-modal__btn--error');
+const modalSuccessBtn = document.querySelector('.form-modal__btn--success');
+const submitBtn = document.querySelector('.form-submit__btn');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  openErrorModal();
+  closeModal();
+})
+
+function openErrorModal() {
+  modalError.classList.add('modal-open');
+  body.classList.add('offScroll');
+};
+
+function openSuccessModal() {
+  modalSuccess.classList.add('modal-open');
+  body.classList.add('offScroll');
+};
+
+function closeModal() {
+  modalBg.forEach(item => {
+    item.addEventListener('click', (e) => {
+      modalError.classList.remove('modal-open');
+      console.log(e);
+    })
+  })
+  //body.classList.remove('offScroll');
+};
+
