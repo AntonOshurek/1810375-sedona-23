edbc0647bc867a6ac464b15fc2e3e915032d76c7
AntonOshurek <oshurekweb@gmail.com>
2021-09-01 15:48:05 +0200

    form submit logick

--- source/js/script.js
+++ source/js/script.js
@@ -25,39 +25,62 @@ closeMenu();
 
 //form modals
 
+const form = document.querySelector('#form');
 const body = document.querySelector('.body');
 const modalError = document.querySelector('.form-modal--error');
 const modalSuccess = document.querySelector('.form-modal--success');
 const modalBg = document.querySelectorAll('.modal__bg');
-
-
-const modalErrorBtn = document.querySelector('.form-modal__btn--error');
-const modalSuccessBtn = document.querySelector('.form-modal__btn--success');
+const modalBtn = document.querySelectorAll('.form-modal__btn');
 const submitBtn = document.querySelector('.form-submit__btn');
+
+//inputs
+
+const email = document.querySelector('#form-email');
+const telephone = document.querySelector('#form-tel');
+console.log(email.validateEmail);
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  openErrorModal();
-  closeModal();
+  if(validateEmail(email.value) != true) {
+    openErrorModal();
+  }else {
+    openSuccessModal();
+    form.submit();
+  }
 })
+
+function validateEmail(email) {
+  const re = /^[a-z0-9_][a-z0-0\._-]*[a-z0-9_]*@([a-z0-9]+[a-z0-9_-]*[a-z0-9]+\.)+[a-z0-9]+$/i;
+  return re.test(String(email).toLowerCase());
+}
 
 function openErrorModal() {
   modalError.classList.add('modal-open');
   body.classList.add('offScroll');
+  closeModal()
 };
 
 function openSuccessModal() {
   modalSuccess.classList.add('modal-open');
   body.classList.add('offScroll');
+  closeModal()
 };
 
 function closeModal() {
   modalBg.forEach(item => {
     item.addEventListener('click', (e) => {
       modalError.classList.remove('modal-open');
-      console.log(e);
+      modalSuccess.classList.remove('modal-open');
+      body.classList.remove('offScroll');
     })
   })
-  //body.classList.remove('offScroll');
+  modalBtn.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      modalError.classList.remove('modal-open');
+      modalSuccess.classList.remove('modal-open');
+      body.classList.remove('offScroll');
+    })
+  })
 };
 
